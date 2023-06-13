5d0c91bb227602b0d9becdaf26d4cffefee59840
minpark96 <hmpark3355@gmail.com>
2023-06-13 23:03:03 +0900

    Fix stack and queue calc

--- 20230612.cpp
+++ 20230612.cpp
@@ -5,13 +5,21 @@
 #include "stack.h"
 #include "queue.h"
 
-void Enque(std::string& temp, Queue& poQue);
+void Enque(std::string& temp, Queue& que);
+void Deque(std::string& temp, Queue& que);
+void DequeAndPush(std::string& temp, Queue& que, Stack& stk);
+void Peek(std::string& temp, Stack& stk);
+void Peek(std::string& temp, Queue& que);
+void Push(std::string& temp, Stack& stk);
+void ClearAndPush(std::string& temp, Stack& stk, char& ch);
+void Pop(std::string& temp, Stack& stk);
+void PopAndEnque(std::string& temp, Stack& stk, Queue& que);
+void PopAndStod(std::string& temp, Stack& stk, double& d);
 
 int main()
 {
 	using namespace std;
 	ifstream inFile;
-
 	inFile.open("calc.txt");
 	if (!inFile.is_open())
 	{
@@ -19,192 +27,194 @@ int main()
 		cout << "프로그램을 종료합니다." << endl;
 		exit(EXIT_FAILURE);
 	}
-	int count = 0;
 
+	int count = 0;
 	string input;
 	while (inFile.good())
 	{
 		count++;
 		inFile >> input;
 	}
+
 	if (inFile.eof())
 		cout << "파일 끝" << endl;
 	else if (inFile.fail())
 		cout << "데이터 불일치" << endl;
 	else
 		cout << "알 수 없는 이유로 입력이 종료" << endl;
+
 	if (count == 0)
 		cout << "데이터 없음" << endl;
 	else
 	{
 		int max = 64;
-		Stack orrStk;
-		Stack ordStk;
-		Queue poQue;
+		Stack stkOrr;
+		Stack stkOrd;
+		Queue quePo;
 
-		if(!orrStk.Initialize(max))
+		if(!stkOrr.Initialize(max))
 			cout << "\a오류: 스택 초기화에 실패했습니다." << endl;
-		if(!ordStk.Initialize(max))
+		if(!stkOrd.Initialize(max))
 			cout << "\a오류: 스택 초기화에 실패했습니다." << endl;
-		if(!poQue.Initialize(max))
+		if(!quePo.Initialize(max))
 			cout << "\a오류: 스택 초기화에 실패했습니다." << endl;
 
 		char ch;
 		int idx = 0;
 		string temp;
-		while (idx < input.size() + 2)
+		while (true)
 		{
 			ch = input[idx];
-			if (ch == '*' || ch == '/')
+			if (ch == 0)
 			{
-				Enque(temp, poQue);
+				Peek(temp, stkOrr);
+				if (temp == "*" || temp == "/" || temp == "+" || temp == "-")
+					PopAndEnque(temp, stkOrr, quePo);
+				break;
+			}
 
-				if (orrStk.Peek(temp))
+			if (ch == '*' || ch == '/' || ch == '+' || ch == '-' || ch == '(' || ch == ')')
+			{
+				if (!temp.empty())
+					Enque(temp, quePo);
+
+				if (ch == '*' || ch == '/')
 				{
+					Peek(temp, stkOrr);
 					if (temp == "*" || temp == "/")
-					{
-						if(orrStk.Pop(temp))
-						{
-							if (!poQue.Enque(temp))
-								cout << "\a오류: 인큐에 실패했습니다." << endl;
-						}
-						else
-							cout << "\a오류: 팝에 실패했습니다." << endl;
-					}
+						PopAndEnque(temp, stkOrr, quePo);
+					ClearAndPush(temp, stkOrr, ch);
 				}
-				else
+				else if (ch == '+' || ch == '-')
 				{
-					cout << "\a오류: 스택 피크에 실패했습니다." << endl;
-				}
-
-				temp.clear();
-				temp += ch;
-				if (!orrStk.Push(temp))
-					cout << "\a오류: 푸시에 실패했습니다." << endl;
-				temp.clear();
-			}
-			else if (ch == '+' || ch == '-')
-			{
-				Enque(temp, poQue);
-
-				if (orrStk.Peek(temp))
-				{
+					Peek(temp, stkOrr);
 					if (temp == "*" || temp == "/" || temp == "+" || temp == "-")
-					{
-						if (orrStk.Pop(temp))
-						{
-							if (!poQue.Enque(temp))
-								cout << "\a오류: 인큐에 실패했습니다." << endl;
-						}
-						else
-							cout << "\a오류: 팝에 실패했습니다." << endl;
-					}
+						PopAndEnque(temp, stkOrr, quePo);
+					ClearAndPush(temp, stkOrr, ch);
+				}
+				else if (ch == ')')
+				{
+					Peek(temp, stkOrr);
+					if (temp == "*" || temp == "/" || temp == "+" || temp == "-")
+						PopAndEnque(temp, stkOrr, quePo);
+					Pop(temp, stkOrr);
+					temp.clear();
 				}
 				else
-				{
-					cout << '?' << temp << endl;
-					cout << "\a오류: 스택 피크에 실패했습니다." << endl;
-				}
-
-				temp.clear();
-				temp += ch;
-				if (!orrStk.Push(temp))
-					cout << "\a오류: 푸시에 실패했습니다." << endl;
-				temp.clear();
-			}
-			else if (ch == ')')
-			{
-				Enque(temp, poQue);
-
-				if (orrStk.Pop(temp))
-				{
-					if (!poQue.Enque(temp))
-						cout << "\a오류: 인큐에 실패했습니다." << endl;
-					
-					if(!orrStk.Pop(temp))
-						cout << "\a오류: 팝에 실패했습니다." << endl;
-				}
-				else
-					cout << "\a오류: 스택 팝에 실패했습니다." << endl;
-
-				temp.clear();
-			}
-			else if (ch == '(')
-			{
-				Enque(temp, poQue);
-
-				temp.clear();
-				temp += ch;
-				if (!orrStk.Push(temp))
-					cout << "\a오류: 푸시에 실패했습니다." << endl;
-				temp.clear();
+					ClearAndPush(temp, stkOrr, ch);
 			}
 			else
 				temp += ch;
 			idx++;
 		}
 		
-		while (!poQue.IsEmpty())
+		while (!quePo.IsEmpty())
 		{
 			string temp;
-			if (poQue.Peek(temp))
+			Peek(temp, quePo);
+			if (temp.empty())
+				continue;
+			else if (temp == "*" || temp == "/" || temp == "+" || temp == "-")
 			{
-				if (temp == "*" || temp == "/" || temp == "+" || temp == "-")
-				{
-					if (!poQue.Deque(temp))
-						cout << "\a오류: 디큐에 실패했습니다." << endl;
-					string pop;
-					double ord1, ord2, res;
-					if (ordStk.Pop(pop))
-						ord2 = stod(pop);
-					else
-						cout << "\a오류: 팝에 실패했습니다." << endl;
-					if (ordStk.Pop(pop))
-						ord1 = stod(pop);
-					else
-						cout << "\a오류: 팝에 실패했습니다." << endl;
+				Deque(temp, quePo);
+				string pop;
+				double ord1, ord2, res;
+				PopAndStod(pop, stkOrd, ord2);
+				PopAndStod(pop, stkOrd, ord1);
 
-					if (temp == "*")
-						res = ord1 * ord2;
-					else if (temp == "/")
-						res = ord1 / ord2;
-					else if (temp == "+")
-						res = ord1 + ord2;
-					else
-						res = ord1 - ord2;
-
-					temp = to_string(res);
-
-					if (!ordStk.Push(temp))
-						cout << "\a오류: 푸시에 실패했습니다." << endl;
-				}
+				if (temp == "*")
+					res = ord1 * ord2;
+				else if (temp == "/")
+					res = ord1 / ord2;
+				else if (temp == "+")
+					res = ord1 + ord2;
 				else
-				{
-					if(!poQue.Deque(temp))
-						cout << "\a오류: 디큐에 실패했습니다." << endl;
-					if(!ordStk.Push(temp))
-						cout << "\a오류: 푸시에 실패했습니다." << endl;
-				}
+					res = ord1 - ord2;
+
+				temp = to_string(res);
+				Push(temp, stkOrd);
 			}
 			else
-				cout << "\a오류: 큐 피크에 실패했습니다." << endl;
+				DequeAndPush(temp, quePo, stkOrd);
 		}
 
 		string res;
-		if (!ordStk.Pop(res))
-			cout << "\a오류: 팝에 실패했습니다." << endl;
+		Pop(res, stkOrd);
 		cout << "result: " << res << endl;
+
+		stkOrr.Terminate();
+		stkOrd.Terminate();
+		quePo.Terminate();
 	}
 	inFile.close();
 
 	return 0;
 }
 
-void Enque(std::string& temp, Queue& poQue)
+void Enque(std::string& temp, Queue& que)
+{	
+	if (!que.Enque(temp))
+		std::cout << "\a오류: 인큐에 실패했습니다." << std::endl;
+}
+
+void Deque(std::string& temp, Queue& que)
 {
-	if (!temp.empty())
-	{
-		if (!poQue.Enque(temp))
-			std::cout << "\a오류: 인큐에 실패했습니다." << std::endl;
-	}
+	if (!que.Deque(temp))
+		std::cout << "\a오류: 디큐에 실패했습니다." << std::endl;
+}
+
+void DequeAndPush(std::string& temp, Queue& que, Stack& stk)
+{
+	if (!que.Deque(temp))
+		std::cout << "\a오류: 디큐에 실패했습니다." << std::endl;
+	else
+		Push(temp, stk);
+}
+
+void Peek(std::string& temp, Stack& stk)
+{
+	if (!stk.Peek(temp))
+		std::cout << "\a오류: 스택 피크에 실패했습니다." << std::endl;
+}
+
+void Peek(std::string& temp, Queue& que)
+{
+	if (!que.Peek(temp))
+		std::cout << "\a오류: 큐 피크에 실패했습니다." << std::endl;
+}
+
+void Push(std::string& temp, Stack& stk)
+{
+	if (!stk.Push(temp))
+		std::cout << "\a오류: 푸시에 실패했습니다." << std::endl;
+}
+
+void ClearAndPush(std::string& temp, Stack& stk, char& ch)
+{
+	temp.clear();
+	temp += ch;
+	Push(temp, stk);
+	temp.clear();
+}
+
+void Pop(std::string& temp, Stack& stk)
+{
+	if (!stk.Pop(temp))
+		std::cout << "\a오류: 팝에 실패했습니다." << std::endl;
+}
+
+void PopAndEnque(std::string& temp, Stack& stk, Queue& que)
+{
+	if (!stk.Pop(temp))
+		std::cout << "\a오류: 팝에 실패했습니다." << std::endl;
+	else
+		Enque(temp, que);
+}
+
+void PopAndStod(std::string& temp, Stack& stk, double& d)
+{
+	if (!stk.Pop(temp))
+		std::cout << "\a오류: 팝에 실패했습니다." << std::endl;
+	else
+		d = stod(temp);
 }
